ed98cdd30581e67cd8241b62bb4928fb9776e3b9
arseny <arsenyeghiazarian@gmail.com>
2018-05-26 18:33:00 +0400

    project installed

---
+++ time-tracker/src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimerComponent } from './components/timer/timer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TimerComponent]
+})
+export class PagesModule { }
